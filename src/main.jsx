a26435fb775372da0359399a2bb957153893d20a
Shauryaparatpsingh001 <shauryapsingh000@gmail.com>
2024-12-07 18:15:22 +0530

    Minnor Change

--- src/main.jsx
+++ src/main.jsx
@@ -15,51 +15,51 @@ import { Skills } from "./components/Skills/Skills";
 import { Login } from "./components/Login/Login";
 import { Signup } from "./components/Signup/Signup";
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />,
-    children: [
-      {
-        index: true, //  path:"/Home",
-        element: <Home />,
-      },
+// const router = createBrowserRouter([
+//   {
+//     path: "/",
+//     element: <App />,
+//     children: [
+//       {
+//         index: true, //  path:"/Home",
+//         element: <Home />,
+//       },
 
-      {
-        path: "About",
-        element: <About />,
-      },
-      {
-        path: "Contact",
-        element: <Contact />,
-      },
-      {
-        path: "Skills",
-        element: <Skills />,
-      },
-      {
-        path: "Login",
-        element: <Login />,
-      },
-      {
-        path: "Signup",
-        element: <Signup />,
-      },
-    ],
-  },
-]);
-// const router= createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<App />}>
-//         <Route index element ={<Home/>} />
-//         <Route path="About" element={<About/>} />
-//         <Route path="Contact" element={<Contact/>} />
-//         <Route path="Skills" element={<Skills/>} />
-//         <Route path="Login" element={<Login/>} />
-//         <Route path="Signup" element={<Signup/>} />
-//     </Route>
-//   )
-// )
+//       {
+//         path: "About",
+//         element: <About />,
+//       },
+//       {
+//         path: "Contact",
+//         element: <Contact />,
+//       },
+//       {
+//         path: "Skills",
+//         element: <Skills />,
+//       },
+//       {
+//         path: "Login",
+//         element: <Login />,
+//       },
+//       {
+//         path: "Signup",
+//         element: <Signup />,
+//       },
+//     ],
+//   },
+// ]);
+const router= createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<App />}>
+        <Route index element ={<Home/>} />
+        <Route path="About" element={<About/>} />
+        <Route path="Contact" element={<Contact/>} />
+        <Route path="Skills" element={<Skills/>} />
+        <Route path="Login" element={<Login/>} />
+        <Route path="Signup" element={<Signup/>} />
+    </Route>
+  )
+)
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
